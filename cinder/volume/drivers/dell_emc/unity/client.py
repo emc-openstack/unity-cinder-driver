openstack/cinder

[Unity] Compressed volume support
Support operations are:

Create/delete/clone/extend/attach/detach compressed volumes

Change-Id: I43d840875ebb99744d23d87482603e34551c3882
Implements: blueprint unity-compressed-volume-support

cinder/volume/drivers/dell_emc/unity/client.py
@@ -57,7 +57,8 @@ class UnityClient(object):
         return self.system.serial_number
 
     def create_lun(self, name, size, pool, description=None,
-                   io_limit_policy=None, is_thin=None):
+                   io_limit_policy=None, is_thin=None,
+                   is_compressed=None):
         """Creates LUN on the Unity system.
 
         :param name: lun name
@@ -66,13 +67,15 @@ class UnityClient(object):
         :param description: lun description
         :param io_limit_policy: io limit on the LUN
         :param is_thin: if False, a thick LUN will be created
+        :param is_compressed: is compressed LUN enabled
         :return: UnityLun object
         """
         try:
             lun = pool.create_lun(lun_name=name, size_gb=size,
                                   description=description,
                                   io_limit_policy=io_limit_policy,
-                                  is_thin=is_thin)
+                                  is_thin=is_thin,
+                                  is_compression=is_compressed)
         except storops_ex.UnityLunNameInUseError:
             LOG.debug("LUN %s already exists. Return the existing one.",
                       name)

emc-openstack/unity-cinder-driver

(cherry picked from commit 2da949da1f79a0121d75c50eecdd102382287bda)

cinder/volume/drivers/dell_emc/unity/client.py
@@ -57,7 +57,8 @@ class UnityClient(object):
         return self.system.serial_number
 
     def create_lun(self, name, size, pool, description=None,
-                   io_limit_policy=None, cg_name=None):
+                   io_limit_policy=None, is_compressed=None,
+                   cg_name=None):
         """Creates LUN on the Unity system.
 
         :param name: lun name
@@ -65,12 +66,14 @@ class UnityClient(object):
         :param pool: UnityPool object represent to pool to place the lun
         :param description: lun description
         :param io_limit_policy: io limit on the LUN
+        :param is_compressed: is compressed LUN enabled
         :return: UnityLun object
         """
         try:
             lun = pool.create_lun(lun_name=name, size_gb=size,
                                   description=description,
-                                  io_limit_policy=io_limit_policy)
+                                  io_limit_policy=io_limit_policy,
+                                  is_compression=is_compressed)
         except storops_ex.UnityLunNameInUseError:
             LOG.debug("LUN %s already exists. Return the existing one.",
                       name)